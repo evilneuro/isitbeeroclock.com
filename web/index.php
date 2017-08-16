b0c26563db89f8a290ef4f46f921c8246ea16a8b
William Anderson <neuro@well.com>
2017-08-16 15:03:32 +0100

    bootstrap 2.0.1 -> 3.3.7

--- web/index.php
+++ web/index.php
@@ -4,12 +4,16 @@ ini_set('display_errors','1');
 include "assets/inc/page-start.inc";
 ?>
 
-<div class="row"><div class="span12" style="height: 100%; vertical-align: middle; padding-top: 200px;">
-<div class="row"><div class="span12" style="text-align: center">
+<div class="row">
+<div class="col-xs-12" style="height: 100%; vertical-align: middle; padding-top: 200px;">
+<div class="row">
+<div class="col-xs-12" style="text-align: center">
 <h1 class="jumbotron"><a href="http://beeroverip.org/random/" title="YES - click for a Beer-over-IP">YES</a></h1>
 <p class="subtitle"><a href="http://beeroverip.org/random/">hint: it's <strong>always</strong> beer o'clock</a></p>
-</div></div>
-</div></div>
+</div>
+</div>
+</div>
+</div>
 
 <?php
 include "assets/inc/page-stop.inc";
