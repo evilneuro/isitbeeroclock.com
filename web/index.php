569600082a356872e1281ce60a858291ef1f6c06
William Anderson <neuro@well.com>
2017-08-17 12:50:47 +0100

    change random link to functioning link

--- web/index.php
+++ web/index.php
@@ -8,8 +8,8 @@ include "assets/inc/page-start.inc";
 <div class="col-xs-12" style="height: 100%; vertical-align: middle; padding-top: 200px;">
 <div class="row">
 <div class="col-xs-12" style="text-align: center">
-<h1 class="jumbotron"><a href="http://beeroverip.org/random/" title="YES - click for a Beer-over-IP">YES</a></h1>
-<p class="subtitle"><a href="http://beeroverip.org/random/">hint: it's <strong>always</strong> beer o'clock</a></p>
+<h1 class="jumbotron"><a href="http://beeroverip.org/" title="YES - click for a Beer-over-IP">YES</a></h1>
+<p class="subtitle"><a href="http://beeroverip.org/">hint: it's <strong>always</strong> beer o'clock</a></p>
 </div>
 </div>
 </div>
